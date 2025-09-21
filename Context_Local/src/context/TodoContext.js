ae5976c124a099497594687dbed5b40affa4d493
RajaBijoriya <rajabijoriya875@gmail.com>
2025-09-21 18:55:38 +0530

    Todo

---
+++ Context_Local/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const TodoContext = createContext({
+    todos : [
+        {
+            id: 1,
+            todo: "Todo Msg",
+            complete: false,
+        
+        }
+    ],
+    addTodo : (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id)=> {},
+    toggleComplete: (id) => {}
+});
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TotoProvider = TodoContext.Provider
---
+++ Context_Local/src/context/index.js
@@ -0,0 +1,1 @@
+export {TodoContext, TotoProvider, useTodo} from "./TodoContext"
